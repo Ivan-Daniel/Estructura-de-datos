61d4d51905671411e811bf69560981986d8db264
Ivan Daniel <87994678+Ivan-Daniel@users.noreply.github.com>
2023-02-05 21:13:45 -0500

    Delete KOTLIN/Holamundo directory

--- KOTLIN/Holamundo/src/main/kotlin/Suma/suma.kt
+++
@@ -1,9 +0,0 @@
-package Suma
-fun main() {
-    print("Ingrese el primer valor: ")
-    val a = readLine()!!.toInt()
-    print("Ingrese el segundo valor: ")
-    val b = readLine()!!.toInt()
-    val suma = a + b
-    println("la suma de $a entre $b es $suma")
-}
